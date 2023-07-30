f7e1a101af95181592cfed6335619bd83fbcea56
se93hr <140147249+se93hr@users.noreply.github.com>
2023-07-30 23:25:38 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x368b181ab574aac0e31b25bad95249a9d271babd";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
